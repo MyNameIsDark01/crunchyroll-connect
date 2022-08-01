3f759e9eb4284c7ebe9cf27881d4076a190ca0a7
MyNameIsDark01 <48729254+MyNameIsDark01@users.noreply.github.com>
2022-08-01 08:30:25 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="crunchyroll-beta",
+    version="1.4.3",
+    author="mynameisdark01",
+    description="API for Crunchyroll BETA",
+    url="https://github.com/MyNameIsDark01/crunchyroll-connect",
+    project_urls={
+        "Tracker": "https://github.com/MyNameIsDark01/crunchyroll-connect/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "requests",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
