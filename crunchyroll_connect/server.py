bde0a9dac8737037df6377ce25098d3ce2f0108f
stefanodvx <69367859+stefanodvx@users.noreply.github.com>
2022-08-28 15:20:04 +0200

    Update server.py

--- crunchyroll_connect/server.py
+++ crunchyroll_connect/server.py
@@ -451,7 +451,7 @@ class CrunchyrollServer:
             'limit': limit,
             'offset': offset,
             'filter': filter,
-            'locale': "enUS"
+            'locale': self.locale
         }
 
         response = self.session.get(url, params=data, cookies=self.session.cookies).json()
